5fd82f908f64398ccee59abaeaf4502af351a5d8
Cade M <you@example.com>
2022-03-18 12:28:05 -0500

    fixed extra quote

--- scriptHelper.js
+++ scriptHelper.js
@@ -57,7 +57,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     launchStatus.style.color = 'rgb(199, 37, 78)';
 
   } else if (fuelLevel >= 10000 && cargoLevel > 10000) {
-    fuelStatus.innerHTML = "Fuel level high enough for launch'";
+    fuelStatus.innerHTML = "Fuel level high enough for launch";
     cargoStatus.innerHTML = "Cargo mass too heavy for launch";
     launchStatus.innerHTML = "Shuttle Not Ready for Launch";
     launchStatus.style.color = 'rgb(199, 37, 78)';
